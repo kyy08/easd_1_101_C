1f079ef0be2cc61ea7f373e8b77f43bab480c6f4
kyy08 <kyyelzz1@gmail.com>
2023-04-06 15:10:01 +0700

    selection sort

--- easd_1_101_C/easd_1_101_C.cpp
+++ easd_1_101_C/easd_1_101_C.cpp
@@ -1,4 +1,45 @@
 #include <iostream>
 using namespace std;
 
+int arr[21];				//
+int cmp_count;				//
+int mov_count;				//
+int n;
 
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang Element Array: ";
+		cin >> n;
+
+		if (n <= 21)
+			break;
+		else
+			cout << "\nMaksimum Panjang Array adalah 21" << endl;
+	}
+
+	cout << "\n===================" << endl;
+	cout << "\nEnter Element Array" << endl;
+	cout << "\n===================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
+
+void swap(int x, int y)
+{
+	int temp;
+
+	temp = arr[j];
+	arr[j] = arr[min_index];
+	arr[min_index] = temp;
+}
+
+void S_sort(int low, int high)
+{
+	int pivot, i, j;
+	if (low > high) //langkah algoritma no.1
+		return;
